c575e6976b6cf0cd06027ec962b1b978e27cb712
Carl Riis <carltheperson@protonmail.com>
2021-09-15 09:52:22 +0200

    Build bash script

---
+++ create-build.sh
@@ -0,0 +1,7 @@
+wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+
+echo export PATH=$PATH:/usr/local/go/bin > ~/.profile
+
+source ~/.profile
+
+go version
